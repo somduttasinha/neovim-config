151c32a4f78202bbad4870c089f7a5e2088ad227
Som Sinha <somdsinha@gmail.com>
2023-07-10 18:42:26 +0200

    initial commit

---
+++ lua/somsinha/core/options.lua
@@ -0,0 +1,41 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursorline
+opt.cursorline = false -- YouTuber had it as true
+
+-- appearance
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "no" -- YouTuber had it as yes
+
+-- backspace
+
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
