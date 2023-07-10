3f621940947c62d4e7a2173d94380deb36749cd2
Som Sinha <somdsinha@gmail.com>
2023-07-10 19:30:41 +0200

    solve mason bugs

--- lua/somsinha/plugins/lsp/mason.lua
+++ lua/somsinha/plugins/lsp/mason.lua
@@ -13,6 +13,13 @@ if not mason_lspconfig_status then
 end
 
 
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+
 mason.setup()
 
 
